bc3939862b0afc1d3ec177d3dfca81ac57c2599d
Nicole Sedillo <ntsedillo@live.ca>
2024-04-24 21:17:17 -0700

    removed mouse in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -100,14 +100,10 @@ function Home() {
                 </section>
                 
                 <div class="mouse_scroll">
-                    <div class="mouse">
-                        <div class="wheel"></div>
-                        </div>
-                        <div>
                         <span class="m_scroll_arrows one"></span>
                         <span class="m_scroll_arrows two"></span>
                         <span class="m_scroll_arrows three"></span>
-                        </div>
+                        
                     </div>
             </header>
 
